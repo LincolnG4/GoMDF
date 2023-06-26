b833a38ff6802663693bbabeac109934608de30f
LincolnG4 <g97santos@gmail.com>
2023-06-26 23:18:21 +0200

    Update Tasks

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # GoASAMMDF
 ASAM MDF / MF4 (Measurement Data Format) files editor in GoLang
 
-Targets / Tasks :
- [] Read MF4 Files
- [] Read any version of MDF file 
- [] Otimize
- [] Write MF4 Files
- [] Write any version of MDF file
- [] Otimize
-
+## **Targets / Tasks** :  
+ [ ] Read MF4 Files  
+ [ ] Read any version of MDF file  
+ [ ] Otimize  
+ [ ] Write MF4 Files  
+ [ ] Write any version of MDF file  
+ [ ] Otimize  
---
+++ src/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+)
+
+type IDBlock struct {
+	IDFile          [8]byte
+	IDVersion       [8]byte
+	IDProgram       [8]byte
+	IDReserved1     [4]byte
+	IDVersionNumber uint16
+	IDReserved2     [34]byte
+}
+
+type HDBlock struct {
+	ID          [4]byte
+	Reserved    [4]byte
+	Length      uint64
+	LinkCount   uint64
+	HDDGFirst   uint64
+	HDFHFirst   uint64
+	HDCHFirst   uint64
+	HDATFirst   uint64
+	HDEVFirst   uint64
+	HDMDComment uint64
+	StartTime   uint64
+	TZOffset    int16
+	DSTOffset   int16
+	TimeFlags   uint8
+	TimeClass   uint8
+	Flags       uint8
+	Reserved2   uint8
+	StartAngle  float32
+	StartDist   float32
+	Reserved3   byte
+}
+
+func errorHandler(err error) {
+	if err != nil {
+		if err != io.EOF {
+			fmt.Println(err)
+		}
+
+	}
+}
+
+func (idBlock *IDBlock) init(reader *bufio.Reader) {
+
+	IDBLOCK_SIZE := 64
+
+	buf := make([]byte, IDBLOCK_SIZE)
+
+	_, err := reader.Read(buf)
+
+	errorHandler(err)
+
+	buffer := bytes.NewBuffer(buf)
+	errs := binary.Read(buffer, binary.LittleEndian, &(*idBlock))
+	if errs != nil {
+		var foo [8]byte
+		idBlock.IDFile = copy(foo[:], "MDF     ")
+		idBlock.IDVersion = copy(foo[:], "4.00    ")
+		idBlock.IDProgram = copy(foo[:], "GoMDF1.0")
+		idBlock.IDReserved1 = bytes.Repeat([]byte{0}, 4)
+		idBlock.IDVersionNumber = 400
+		idBlock.IDReserved2 = bytes.Repeat([]byte{0}, 34)
+	}
+}
+
+func (hdBlock *HDBlock) init(reader *bufio.Reader) {
+
+	HDBLOCK_SIZE := 104
+
+	buf := make([]byte, HDBLOCK_SIZE)
+
+	_, err := reader.Read(buf)
+
+	errorHandler(err)
+
+	buffer := bytes.NewBuffer(buf)
+	fmt.Println(string(buf))
+	errs := binary.Read(buffer, binary.LittleEndian, &(*hdBlock))
+
+	fmt.Println((hdBlock.ID[:]))
+	if errs != nil {
+		id := "##HD"
+		var byteArr [4]byte
+		for i := 0; i < len(id); i++ {
+			byteArr[i] = id[i]
+		}
+		hdBlock.ID = byteArr
+
+		hdBlock.Length = 0
+		hdBlock.LinkCount = 0
+		hdBlock.HDDGFirst = 0
+		hdBlock.HDFHFirst = 0
+		hdBlock.HDCHFirst = 0
+		hdBlock.HDATFirst = 0
+		hdBlock.HDEVFirst = 0
+		hdBlock.HDMDComment = 0
+		hdBlock.StartTime = 0
+		hdBlock.TZOffset = 0
+		hdBlock.DSTOffset = 0
+		hdBlock.TimeFlags = 0
+		hdBlock.TimeClass = 0
+		hdBlock.Flags = 0
+		hdBlock.Reserved2 = 0
+		hdBlock.StartAngle = 0
+		hdBlock.StartDist = 0
+		hdBlock.Reserved3 = 0
+	}
+
+}
+
+func main() {
+
+	file, err := os.Open("samples/sample3.mf4")
+	errorHandler(err)
+	defer file.Close()
+
+	//Create IDBLOCK
+	idBlock := IDBlock{}
+	_, err = file.Seek(0, 0)
+
+	reader := bufio.NewReader(file)
+
+	idBlock.init(reader)
+
+	if idBlock.IDVersion > 400 {
+		hdBlock := HDBlock{}
+		_, err = file.Seek(0x40, 0)
+		hdBlock.init(reader)
+	}
+}
